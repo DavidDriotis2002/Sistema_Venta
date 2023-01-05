b8a4e5bed861dc9921d30d620fc1448315b4e5e5
DavidDriotis <daviddriotis2002@gmail.com>
2023-01-04 23:15:22 -0600

    Actualización 04/01/2023
    - Creación clase conexion

---
+++ SistemaVenta/CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    class Conexion
+    {
+        public static string Cn = "Data Source=LAPTOP-NI0D2KGL\\SQLEXPRESS; Initial Catalog=dbventas; Integrated Security=true";
+    }
+}
