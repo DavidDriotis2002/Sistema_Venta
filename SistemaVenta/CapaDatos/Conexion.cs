5a14140783d5412ae79f0413fd5f3ab476546611
Johny Driotis <johny.driotis1996@gmail.com>
2023-01-29 19:19:37 -0600

    Actualización 29/01/2023
    - Se agrega proyecto de pencil para mockups

--- SistemaVenta/CapaDatos/Conexion.cs
+++ SistemaVenta/CapaDatos/Conexion.cs
@@ -8,6 +8,6 @@ namespace CapaDatos
 {
      class Conexion
     {
-        public static string Cn = "Data Source=LAPTOP-NI0D2KGL\\SQLEXPRESS; Initial Catalog=dbventas; Integrated Security=true";
+        public static string Cn = "Data Source=DESKTOP-1KC49PC; Initial Catalog=dbventas; Integrated Security=true";
     }
 }
